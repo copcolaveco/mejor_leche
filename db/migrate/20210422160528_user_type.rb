3c02e8918a0933f2247259769621454c1917bb73
itcolaveco <it.colaveco@gmail.com>
2021-05-13 10:49:27 -0300

    db migrate

--- db/migrate/20210422160528_user_type.rb
+++
@@ -1,4 +0,0 @@
-class UserType < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
--- db/migrate/20210426022228_change_user.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUser < ActiveRecord::Migration[6.1]
-  def change
-  	change_column :users, :user_type, :integer
-  end
-end
