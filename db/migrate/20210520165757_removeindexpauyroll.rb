38c1698aa4d556c254ccab16f9a7bde2a1efc397
itcolaveco <it.colaveco@gmail.com>
2021-05-31 10:35:16 -0300

    Delete 20210520165757_removeindexpauyroll.rb

--- db/migrate/20210520165757_removeindexpauyroll.rb
+++
@@ -1,8 +0,0 @@
-class Removeindexpauyroll < ActiveRecord::Migration[6.1]
-  def change
-  	drop_table :months
-  	drop_table :sub_type_user_productors
-  	drop_table :sub_type_user_tecnicos
-  	drop_table :sub_user_types
-  end
-end
