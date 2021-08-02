b9dcac9ed9283fd5495004510d0b9d2d7a1e7879
itcolaveco <it.colaveco@gmail.com>
2021-08-02 15:15:16 -0300

    rename heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'https://guarded-ravine-92354.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://mejorleche.herokuapp.com/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
