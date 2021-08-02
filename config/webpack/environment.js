a5403fc0a13b4a81a520da312c2f58e804827045
itcolaveco <it.colaveco@gmail.com>
2021-08-02 12:08:59 -0300

    details environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,18 @@
+
+
 const { environment } = require('@rails/webpacker')
+
+// To get an overview, have a look at `environment.loaders`.
+console.log(environment.loaders)
+
+const { merge } = require('webpack-merge')
+const sassConfig = environment.loaders.find(el => el.key == 'sass')
+const sassLoader = sassConfig.value.use.find(el => el.loader == 'sass-loader')
+
+sassLoader.options = merge(sassLoader.options, {
+  sassOptions: {
+    indentedSyntax: true
+  }
+})
 
 module.exports = environment
