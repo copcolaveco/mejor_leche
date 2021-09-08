4dd09cfbccc526a735561a45fa33557c3494a99f
itcolaveco <it.colaveco@gmail.com>
2021-09-08 13:40:23 -0300

    funcion ajax en report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,12 +1,15 @@
 class ReportsController < ApplicationController
 
     def index
-        @payrolls = Payroll.where(:estate_id => params[:estate_id])
+        @payrolls = Payroll.where(:estate_id => params[:estate_selection])
+        
         @p = Payroll.where(:estate_id => 1)
     end
 
     def estate_selection
-        @payrolls = Payroll.where(:estate_id => :estate_id)
+        @estate = params[:estate_selection]
+        @payrolls = Payroll.where(:estate_id => @estate).all
+        render :json => @payrolls.map{|c| [c.id, c.cell_count]}
     end
 
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,3 +21,35 @@ ActiveStorage.start()
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require jquery_ujs
+
+
+$(document).ready(function() {
+
+    // #first_select is the id of our first select box, if the ajax request has been successful,
+    // an ajax:success event is triggered.
+  
+    $('#estate_selection').on('ajax:success', function(evt, data, status, xhr) {
+      // get second selectbox by its id
+      var char_cell_count = $('#cell_count');
+  
+      // empty it
+      char_cell_count.empty();
+  
+      // we got a JSON array in data, iterate through it
+  
+      $.each(data, function(index, value) {
+        // append an option
+        var opt = $('<option/>');
+  
+        // value is an array: [:id, :name]
+        opt.attr('value', value[0]);
+        // set text
+        opt.text(value[1]);
+        // append to select
+        opt.appendTo(char_cell_count);
+      });
+    });
+  
+  });
