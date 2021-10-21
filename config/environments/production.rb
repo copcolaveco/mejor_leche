aad03db4e32d18e9441cad4467159b353e23155e
copcolaveco <it@colaveco.com.uy>
2021-10-21 11:29:42 -0300

    Revert "config production"
    This reverts commit 1218b22700b5fc9470bc7e50421de98e4261aa87.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--use-gl=egl'],
-  });
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
